1f4e4f45d1af06068c981e15675f45d41da8e4e8
Jimenez Santiago <santiagovalentinjimenez@gmail.com>
2026-06-02 12:58:40 -0300

    ej2

--- Program.cs
+++ Program.cs
@@ -1,34 +1,61 @@
-﻿Console.WriteLine("Hello, World!");
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+﻿//ejercicio 2
 
-//ejercicio 1
-Console.WriteLine("ingrese un numero: ");
-string n=Console.ReadLine();
-if(int.TryParse(n,out a))
-{
-    if(a>0)
+int a,b,z,d;
+float x;
+do
+{    
+    Console.WriteLine("ingrese numero: ");
+    string n1=Console.ReadLine();
+    float.TryParse(n1, out x);
+
+    Console.WriteLine("que operacion quiere hacer: ");
+    Console.WriteLine("1. valor absoluto");
+    Console.WriteLine("2. cuadrado");
+    Console.WriteLine("3. raiz cuadrada");
+    Console.WriteLine("4. seno");
+    Console.WriteLine("5. coseno");
+    Console.WriteLine("6. parte entera de tipo float\n");
+    string n3=Console.ReadLine();
+    int.TryParse(n3, out z);
+    switch (z)
     {
-        int invertido=0;
-        while(a!=0)
-        {
-            int resto=a%10;
-            invertido=invertido*10+resto;
-            a=a/10;
-        }
-        Console.WriteLine("numero: " +n);
-        Console.WriteLine("numero invertido " +invertido);
+        case 1: 
+            Console.WriteLine($"valor absoluto: {Math.Abs(x)}");
+        break;
+        case 2: 
+            Console.WriteLine($"cuadrado: {Math.Pow(x,2)}");
+        break;
+        case 3: 
+            Console.WriteLine($"raiz cuadrada: {Math.Sqrt(x)}");
+        break;
+        case 4: 
+            Console.WriteLine($"seno: {Math.Sin(x)}");
+        break;
+        case 5: 
+            Console.WriteLine($"coseno: {Math.Cos(x)}");
+        break;
+        case 6: 
+            Console.WriteLine($"parte entera: {Math.Truncate(x)}\n");
+        break;
     }
-    else
-    {
-        Console.WriteLine("debe ser mayor a 0");
-    }
-}
-else
-{
-    Console.WriteLine("no es un numero");
-}
+    Console.WriteLine("ingrese numero n1: ");
+    string n2=Console.ReadLine();
+    int.TryParse(n2, out a);
+    Console.WriteLine("ingrese numero n2 : ");
+    string ne=Console.ReadLine();
+    int.TryParse(ne, out b);
+    Console.WriteLine($"el maximo entre esos num: {Math.Max(a,b)}");
+    Console.WriteLine($"el minimo entre esos num: {Math.Min(a,b)}");
+    Console.WriteLine("desea hacer otra operacion? (1. si | 0. no)");
+    string n4=Console.ReadLine();
+    int.TryParse(n4,out d);
+}while(d!=0);
+
+
+
+
+
+
+
+
+
